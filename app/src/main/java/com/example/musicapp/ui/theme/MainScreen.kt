9101ae31838974e9823313b94f75340d5b29c67a
Chayapol Chaipongsawalee <chayapol.c@arise.tech>
2024-08-12 01:46:10 +0700

    extract main bottom sheet

--- app/src/main/java/com/example/musicapp/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/musicapp/ui/theme/MainScreen.kt
@@ -1,13 +1,7 @@
 package com.example.musicapp.ui.theme
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,7 +12,6 @@ import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Scaffold
@@ -27,7 +20,6 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.primarySurface
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material.rememberScaffoldState
 
@@ -40,11 +32,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.musicapp.R
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -112,7 +102,7 @@ fun MainView() {
             topEnd = roundedCornerRadius
         ),
         sheetContent = {
-            MoreBottomSheet(modifier = modifier)
+            MainBottomSheet(modifier = modifier)
         }
     ) {
         Scaffold(
@@ -186,46 +176,3 @@ fun MainView() {
     }
 
 }
-
-@Composable
-fun MoreBottomSheet(modifier: Modifier) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(300.dp)
-            .background(MaterialTheme.colors.primarySurface)
-    ) {
-        Column(
-            modifier = modifier.padding(16.dp),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Row(modifier = modifier.padding(16.dp)) {
-                Icon(
-                    modifier = Modifier.padding(end = 8.dp),
-                    painter = painterResource(R.drawable.ic_setting),
-                    contentDescription = "setting"
-                )
-                Text(text = "Setting", fontSize = 20.sp, color = Color.White)
-            }
-
-            Row(modifier = modifier.padding(16.dp)) {
-                Icon(
-                    modifier = Modifier.padding(end = 8.dp),
-                    painter = painterResource(R.drawable.ic_share),
-                    contentDescription = "share"
-                )
-                Text(text = "Share", fontSize = 20.sp, color = Color.White)
-            }
-
-            Row(modifier = modifier.padding(16.dp)) {
-                Icon(
-                    modifier = Modifier.padding(end = 8.dp),
-                    painter = painterResource(R.drawable.ic_help),
-                    contentDescription = "help"
-                )
-                Text(text = "Help", fontSize = 20.sp, color = Color.White)
-            }
-        }
-    }
-}
-
